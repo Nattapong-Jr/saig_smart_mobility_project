b16c2fadfe30d86c8cc1760c818c7d6e1ebf6bab
Nattapong-Jr <68030135@kmitl.ac.th>
2026-07-12 23:50:16 +0700

    train first random forest model for accident severity prediction

--- feature__engineering.py
+++ feature__engineering.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 df = pd.read_csv("cleaned_data.csv")
 
@@ -53,3 +56,18 @@ for col in ["time_period", "day_of_week", "weather", "cause_grouped", "road_shap
 
 print(df_model.head())
 print("\nขนาดข้อมูลพร้มอใช้:", df_model.shape)
+
+X = df_model[feature_columns]
+y = df_model["severity_level"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+y_perd = model.predict(X_test)
+
+print("\n--- ผมทดสอบโมเดล ----")
+print("Accuracy: ", accuracy_score(y_test, y_perd))
+print("\nรายละเอียดเพิ่มเติม: ")
+print(classification_report(y_test, y_perd))
