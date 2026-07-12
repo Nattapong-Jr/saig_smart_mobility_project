e9a01bc0ec08137f7e4f7c68258efb42f5346526
Nattapong-Jr <68030135@kmitl.ac.th>
2026-07-12 15:59:36 +0700

    check accident statistics for different time periods.

--- feature__engineering.py
+++ feature__engineering.py
@@ -9,3 +9,17 @@ df["day_of_week"] = df["time"].dt.day_name()
 df["month"] = df["time"].dt.month
 
 print(df[["time", "hour", "day_of_week", "month"]].head(10))
+
+def get_time_period(hour):
+    if 5 <= hour < 12:
+        return "เช้า"
+    elif 12 <= hour < 17:
+        return "บ่าย"
+    elif 17 <= hour < 21:
+        return "เย็น"
+    else:
+        return "กลางคืน"
+
+df["time_period"] = df["hour"].apply(get_time_period)
+print("\n--- จำนวนอุบัติเหตุแต่ละชาวงเวลา ---")
+print(df["time_period"].value_counts()) 
